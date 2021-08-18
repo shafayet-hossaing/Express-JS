8aa87fd95b7f34e08d6007f65445fa5dbb771429
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-18 19:24:50 +0600

    learnt patial using

--- src/dynamicdata.js
+++ src/dynamicdata.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 const app = express();
 const port = 5000
 
 
+const templatesPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
+
+
 app.set('view engine', 'hbs')
+app.set('views', templatesPath)
+hbs.registerPartials(partialsPath)
 
 app.get("/", function (req, res) {
     res.render('index',{
